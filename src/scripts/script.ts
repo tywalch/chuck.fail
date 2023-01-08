1c80b82530ded282ff56cea44718de435a5d0324
tywalch <tywalch@gmail.com>
2023-01-08 15:39:10 -0500

    working local

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -1,197 +1,272 @@
-const SECOND = 1000;
-const MINUTE = 60 * SECOND;
-const HOUR = 60 * MINUTE;
-
-type GetHoursSinceOptions = {
-  since: number;
+interface DurationTracker {
+  getDuration(): Promise<number>;
+  resetDuration: () => Promise<void>;
 }
 
-function getHoursSince(options: GetHoursSinceOptions) {
-  const { since } = options;
-  const now = Date.now();
-  return Math.ceil(Math.max((now - since) / HOUR));
+interface CelebrationAnimation {
+  start: () => void;
+  stop: () => void;
 }
 
-async function getLastVacationDate() {
-  return Date.now() - (HOUR * 73);
-  // return 17;
-}
-
-type CalculateDelayOptions = {
-  duration: number;
+type TallyCounterOptions = {
+  tally: number;
   count: number;
 }
 
-function calculateDelay(options: CalculateDelayOptions) {
-  const { duration, count } = options;
-  return duration / count;
+interface TallyCounter {
+  set(options: TallyCounterOptions): void;
 }
+
+type TallyDisplayOptions = {
+  max: number;
+}
+
+interface TallyDisplay {
+  init(options: TallyDisplayOptions): TallyCounter;
+}
+
+type SetCountFn = (count: number) => void;
 
 async function sleep(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise<void>(resolve => setTimeout(resolve, ms));
 }
 
-type ApplyTallyMarkCountOptions = {
-  num: number;
-  tally: HTMLImageElement;
+function getRandomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min) ) + min;
 }
 
-function applyTallyMarkCount(options: ApplyTallyMarkCountOptions) {
-  const { num, tally } = options;
+function getRandomColor(colors: string[]) {
+  let index = getRandomNumber(0, colors.length - 1);
+  return colors[index];
+}
 
-  if (isNaN(num) || num < 1 || num > 5) {
-    tally.style.display = 'none';
-  } else {
-    tally.style.display = 'block';
-    tally.src = `./img/tallymark${num}.svg`;
+class VacationTracker implements DurationTracker {
+  private readonly HOUR = 1000 * 60 * 60;
+  private lastVacationDate: number;
+
+  constructor() {
+    this.lastVacationDate = (this.HOUR * 73);
   }
-  
-  return tally;
+
+  private getHoursSince(since: number) {
+    const now = Date.now();
+    this.lastVacationDate = Math.ceil(Math.max((now - since) / this.HOUR));
+    return this.lastVacationDate;
+  }
+
+  private async fetchLastVacationDate() {
+    return Date.now() - this.lastVacationDate;
+  }
+
+  async getDuration() {
+    const lastVacationDate = await this.fetchLastVacationDate();
+    return this.getHoursSince(lastVacationDate);
+  }
+
+  async resetDuration() {
+    this.lastVacationDate = Date.now();
+  }
 }
 
-function createTallyGroupElement() {
-  const group = document.createElement('div');
-  group.className = 'tally-group';
-  return group;
+function createVacationTracker() {
+  return new VacationTracker();
 }
 
-type CreateTallyElementOptions = {
-  num: number;
-}
+function createTallyMark() {
+  type ApplyTallyMarkCountOptions = {
+    num: number;
+    tally: HTMLImageElement;
+  }
 
-function createTallyMarkElement(options: CreateTallyElementOptions) {
-  const { num } = options;
-  const tally = document.createElement('img');
-  tally.alt = `Tallymark`;
-  tally.className = 'tally-marks';
+  function applyTallyMarkCount(options: ApplyTallyMarkCountOptions) {
+    const { num, tally } = options;
 
-  return applyTallyMarkCount({ num, tally });
-}
-
-type PrepareTallyGroupsOptions = {
-  max: number;
-  container: HTMLElement
-}
-
-function prepareTallyGroups(options: PrepareTallyGroupsOptions): HTMLElement[] {
-  const { max, container } = options;
-  const groups: HTMLElement[] = [];
-  const appendCurrent = (group?: HTMLElement) => 
-    currentTallyGroup && container.appendChild(currentTallyGroup);
-
-  let currentTallyGroup = createTallyGroupElement();
-  for (let i = 0; i < max + 1; i++) {
-    if (i % 5 === 0) {
-      currentTallyGroup = createTallyGroupElement();
-      appendCurrent(currentTallyGroup);
+    if (isNaN(num) || num < 1 || num > 5) {
+      tally.style.display = 'none';
+    } else {
+      tally.style.display = 'block';
+      tally.src = `./img/tallymark${num}.svg`;
     }
-    groups.push(currentTallyGroup);
+    
+    return tally;
   }
 
-  return groups;
-}
+  function createTallyGroupElement() {
+    const group = document.createElement('div');
+    group.className = 'tally-group';
+    return group;
+  }
 
-type ApplyTallyMarkOptions = {
-  num: number;
-  group: HTMLElement;
-}
+  type CreateTallyElementOptions = {
+    num: number;
+  }
 
-function applyTallyMark(options: ApplyTallyMarkOptions) {
-  const { num, group } = options;
-  const tally = createTallyMarkElement({ num });
-  group.replaceChildren(tally);
-}
+  function createTallyMarkElement(options: CreateTallyElementOptions) {
+    const { num } = options;
+    const tally = document.createElement('img');
+    tally.alt = `Tallymark`;
+    tally.className = 'tally-marks';
 
-type TallyIntervalOptions = {
-  max: number;
-  interval: number;
-}
+    return applyTallyMarkCount({ num, tally });
+  }
 
-async function* incrementTally(options: TallyIntervalOptions) {
-  const { max, interval } = options;
-  for (let count = 0; count < max; count++) {
-    const tally = (count % 5) + 1;
-    yield { tally, count };
-    await sleep(interval);
+  type PrepareTallyGroupsOptions = {
+    max: number;
+    container: HTMLElement;
+  }
+
+  function prepareTallyGroups(options: PrepareTallyGroupsOptions): HTMLElement[] {
+    const { max, container } = options;
+    const groups: HTMLElement[] = [];
+    const appendCurrent = (group?: HTMLElement) => 
+      currentTallyGroup && container.appendChild(currentTallyGroup);
+
+    let currentTallyGroup = createTallyGroupElement();
+    for (let i = 0; i < max + 1; i++) {
+      if (i % 5 === 0) {
+        currentTallyGroup = createTallyGroupElement();
+        appendCurrent(currentTallyGroup);
+      }
+      groups.push(currentTallyGroup);
+    }
+
+    return groups;
+  }
+
+  type ApplyTallyMarkOptions = {
+    num: number;
+    group: HTMLElement;
+  }
+
+  function applyTallyMark(options: ApplyTallyMarkOptions) {
+    const { num, group } = options;
+    const tally = createTallyMarkElement({ num });
+    group.replaceChildren(tally);
+  }
+
+  type TallyIntervalOptions = {
+    max: number;
+    interval: number;
+  }
+
+  async function* incrementTally(options: TallyIntervalOptions) {
+    const { max, interval } = options;
+    for (let count = 0; count < max; count++) {
+      const tally = (count % 5) + 1;
+      yield { tally, count };
+      await sleep(interval);
+    }
+  }
+
+  async function* decrementTally(options: TallyIntervalOptions) {
+    const { max, interval } = options;
+    for (let count = max - 1; count >= 0; count--) {
+      const tally = (count % 5) - 1;
+      yield { tally, count };
+      await sleep(interval);
+    }
+  }
+
+  return {
+    applyTallyMark,
+    incrementTally,
+    decrementTally,
+    prepareTallyGroups,
   }
 }
 
-async function* decrementTally(options: TallyIntervalOptions) {
-  const { max, interval } = options;
-  for (let count = max - 1; count >= 0; count--) {
-    const tally = (count % 5) - 1;
-    yield { tally, count };
-    await sleep(interval);
-  }
-}
+type TallyMark = ReturnType<typeof createTallyMark>;
+type TallyMarkGenerators = Pick<TallyMark, 'decrementTally' | 'incrementTally'>;
 
 type AppElements = {
+  appContainer: HTMLElement;
   tallyContainer: HTMLElement;
-  counterDisplay: HTMLElement;
-  counterDisplayHours: HTMLHeadingElement
-  reset: HTMLButtonElement;
-  canvas: HTMLCanvasElement;
+  totalContainer: HTMLElement;
+  totalDisplay: HTMLHeadingElement
+  confettiContainer: HTMLElement;
+  resetButton: HTMLButtonElement;
+}
+
+type AppAnimationOptions = {
+  delay: number;
+  duration: number;
 }
 
 type AppOptions = {
-  animation: {
-    delay: number;
-    duration: number;
-  }
+  animation: AppAnimationOptions;
   elements: AppElements;
+  vacationer: DurationTracker;
+  tallymark: TallyMarkGenerators;
+  celebration: CelebrationAnimation;
+  tallyDisplay: TallyDisplay;
 }
 
-async function app(options: AppOptions) {
-  const { elements, animation } = options;
-  
-  const lastVacationDate = await getLastVacationDate();
-  const totalHours = getHoursSince({ 
-    since: lastVacationDate
+async function app({
+  elements,
+  animation,
+  tallymark,
+  vacationer,
+  celebration,
+  tallyDisplay,
+}: AppOptions) {  
+  const totalDuration = await vacationer.getDuration();
+  const display = tallyDisplay.init({
+    max: totalDuration
   });
-
-  const interval = animation.delay / totalHours;
-
-  const updateCount = (count: number) => {
-    elements.counterDisplayHours.textContent = `${count} hrs`;
+  const incrementOptions = {
+    max: totalDuration, 
+    interval: animation.delay / totalDuration
+  };
+  
+  const decrementOptions = {
+    max: totalDuration,
+    interval: incrementOptions.interval / 2
   }
-  
-  const groups = prepareTallyGroups({ 
-    max: totalHours, 
-    container: elements.tallyContainer 
-  });
-  
+
   await sleep(animation.delay);
   
-  for await (const { tally, count } of incrementTally({ max: totalHours, interval })) {
-    const group = groups[count];
-    applyTallyMark({ num: tally, group });
-    updateCount(count);
+  for await (const { tally, count } of tallymark.incrementTally(incrementOptions)) {
+    // const group = groups[count];
+    display.set({count, tally});
+    // tallymark.applyTallyMark({ num: tally, group });
+    // totalDisplay.updateTotal(count);
   }
   
-  elements.reset?.addEventListener('click', async () => {
-    for await (const { tally, count } of decrementTally({ max: totalHours, interval })) {
-      const group = groups[count];
-      applyTallyMark({ num: tally, group });
-      updateCount(count);
+  elements.resetButton?.addEventListener('click', async () => {
+    for await (const { tally, count } of tallymark.decrementTally(decrementOptions)) {
+      // const group = groups[count];
+      // tallymark.applyTallyMark({ num: tally, group });
+      // totalDisplay.updateTotal(count);
+      display.set({count, tally});
     }
+
+    celebration.start();
+    await sleep(9_000);
+
+    // totalDisplay.updateTotal(1);
+    // tallymark.applyTallyMark({ num: 1, group: groups[0] });
+    display.set({count: 1, tally: 1});
+    celebration.stop();
   });
 }
 
 function isAppElements(elements: any): elements is AppElements {
   return elements.tallyContainer instanceof HTMLElement &&
-    elements.counterDisplay instanceof HTMLElement &&
-    elements.counterDisplayHours instanceof HTMLElement &&
-    elements.reset instanceof HTMLButtonElement &&
-    elements.canvas instanceof HTMLCanvasElement;
+    elements.totalContainer instanceof HTMLElement &&
+    elements.totalDisplay instanceof HTMLElement &&
+    elements.resetButton instanceof HTMLButtonElement &&
+    elements.confettiContainer instanceof HTMLElement &&
+    elements.appContainer instanceof HTMLElement;
 }
 
 function getAppElements(): AppElements {
   const elements = {
     tallyContainer: document.getElementById('root'),
-    counterDisplay: document.querySelector('.count-body'),
-    counterDisplayHours: document.querySelector('.count-body-hours'),
-    reset: document.getElementById('btn-reset'),
-    canvas: document.getElementById('celebration'),
+    totalContainer: document.querySelector('.count-body'),
+    totalDisplay: document.querySelector('.count-body-hours'),
+    resetButton: document.getElementById('btn-reset'),
+    confettiContainer: document.getElementsByClassName('confetti-container')[0],
+    appContainer: document.getElementsByTagName('body')[0],
   }
 
   if (isAppElements(elements)) {
@@ -201,10 +276,123 @@ function getAppElements(): AppElements {
   throw new Error('Missing required elements');
 }
 
+type CreateSetCountFnOptions = {
+  totalDisplay: HTMLElement;
+  colors: {
+    success: string;
+    failure: string;
+  }
+}
+
+function createSetCountFn(options: CreateSetCountFnOptions): SetCountFn {
+  const { totalDisplay, colors } = options;
+  return (count: number) => {
+    totalDisplay.style.color = count > 0 
+      ? colors.failure 
+      : colors.success;
+    totalDisplay.textContent = `${count} hrs`;
+  }
+}
+
+type CreateTallyCounterOptions = {
+  groups: HTMLElement[];
+  setDisplay: SetCountFn;
+  applyTallyMark: TallyMark['applyTallyMark'];
+}
+
+function createTallyCounter(options: CreateTallyCounterOptions): TallyCounter {
+  const { applyTallyMark, setDisplay, groups } = options;
+  return {
+    set({ count, tally }) {
+      const group = groups[count];
+      setDisplay(count);
+      applyTallyMark({ num: tally, group });
+    }
+  }
+}
+
+type CreateTallyDisplayOptions = {
+  container: HTMLElement;
+  tallymark: TallyMark;
+  setDisplay: SetCountFn;
+}
+
+function createTallyDisplay(options: CreateTallyDisplayOptions): TallyDisplay {
+  const { container, setDisplay, tallymark } = options;
+  const { applyTallyMark, prepareTallyGroups } = tallymark;
+  const groups: HTMLElement[] = [];
+  return {
+    init(options: TallyDisplayOptions) {
+      const { max } = options;
+      if (groups.length === 0) {
+        groups.push(
+          ...prepareTallyGroups({ container, max })
+        );
+      }
+
+      return createTallyCounter({
+        applyTallyMark,
+        setDisplay,
+        groups,
+      });
+    }
+  }
+}
+
 async function main() {
   try {
     const elements = getAppElements();
-    await app({ elements });
+    const vacationer = createVacationTracker();
+    const tallymark = createTallyMark();
+
+    const colors = {
+      success: "#4caf50",
+      failure: "#f44336",
+      background: "#111111fa",
+    }
+    
+    const celebration: CelebrationAnimation = {
+      start() {
+        elements.confettiContainer.style.zIndex = "1000";
+        elements.resetButton.style.display = "none";
+        elements.confettiContainer.style.display = "block";
+        elements.appContainer.style.backgroundColor = colors.success;
+        console.log('Celebration started');
+      },
+      stop() {
+        elements.confettiContainer.style.zIndex = "-1";
+        elements.resetButton.style.display = "block";
+        elements.confettiContainer.style.display = "none";
+        elements.appContainer.style.backgroundColor = colors.background;
+        console.log('Celebration stopped');
+      }
+    }
+
+    const setDisplay = createSetCountFn({
+      totalDisplay: elements.totalDisplay,
+      colors,
+    });
+
+    const tallyDisplay = createTallyDisplay({
+      container: elements.tallyContainer,
+      tallymark: tallymark,
+      setDisplay,
+    });
+
+    const animation = {
+      delay: 1500, 
+      duration: 2000,
+    };
+
+    await app({
+      elements, 
+      tallymark,
+      animation,
+      vacationer,
+      celebration,
+      tallyDisplay,
+    });
+
   } catch (err) {
     console.error('This app is garbage', err);
   }
